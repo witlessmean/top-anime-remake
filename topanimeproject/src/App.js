5f2d124e452959e9094db09c9448dfa0a9905b61
Shawn Elston <elstonshawn1@yahoo.com>
2020-09-24 01:04:12 -0700

    working on api call

--- topanimeproject/src/App.js
+++ topanimeproject/src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { topAirAni } from "./utils/Api";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { createContext } from "react";
+import appStyles from "./appStyles.module.css";
+import animeContainer from "./components/AnimeContainer";
 
 function App() {
+  
+  const aniAPI = createContext()
+  
+  const [pic, setPic] = useState([]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* {pic.map((topPic) => {
+        return <img src={topPic.image_url} key={uuidv4()} />;
+      })} */}
     </div>
   );
 }
---
+++ topanimeproject/src/components/AnimeContainer.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import animeContainerStyles from "./animeContainerStyles.module.css"
+
+const animeContainer = () => {
+    return (
+        <div className="container">
+            
+        </div>
+    )
+}
+
+export default animeContainer
