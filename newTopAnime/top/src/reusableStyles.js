eb5417efdc01e065c3dfb61085129a23322cb142
Shawn Elston <elstonshawn1@yahoo.com>
2020-11-06 12:25:32 -0800

    added syntax extension for styled-components and random notes.

--- newTopAnime/top/src/reusableStyles.js
+++ newTopAnime/top/src/reusableStyles.js
@@ -3,7 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 html {
-    box-sizing: border-box;
+  font-size: 62.5%;  
+  box-sizing: border-box;
   }
   *, *:before, *:after {
     box-sizing: inherit;
@@ -17,7 +18,15 @@ html {
   align-items: center;
   flex-wrap: wrap;
   img{
-    width: 10vw;
+    width: 9vw ;
+    max-width: 100%;
+    height: auto;
     border-radius: 8px
   }
   `;
+
+  //we will build out the container. i want the images as large as they can be. what we will do to compensate for this, is build out the container that they're all in to be the same size, and we will hide overflow. 
+
+  //also we need to be able to let the user know what kind of anime he's looking at AFTER he clicks on the button. and so with that being said, we will need to add an argument to the button so that when you click for example, the 'special' button, it will write special somewhere in big letters to let you know that's where you are. 
+
+  //to the container we will need to add the name, how many episodes it has etc. 
